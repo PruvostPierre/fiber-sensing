7008806fea41fa947ca3988dfb273c0d9a3c6454
Elie Awwad <elie.awwad@telecom-paris.fr>
2025-05-27 17:59:33 +0200

    Edit rxSensingCorrelation.m

--- functions/rxSensingCorrelation.m
+++ functions/rxSensingCorrelation.m
@@ -13,20 +13,20 @@ global Erx;
 
 tic;
 %Correlation process per multiple small blocks. The block size must be specified (p.corrPerBlockTwin, in seconds, with p.corrPerBlockTwin>>length(gCode))
-brutBlockLen = pow2(floor(log2(p.rx.corrPerBlockTwin*p.tx.fSymb+length(p.tx.gCode)-1)));
+brutBlockLen = pow2(floor(log2(p.rx.corrPerBlockTwin*p.tx.fSymb+length(p.rx.gCode)-1)));
 %Test if blocklen is accurately computed (case short t_probing)
-if p.rx.ErxLen < brutBlockLen %MODEL case (mostly) : if fiber+nbcodes too small (as if T_probing << 1s)
-    fprintf('\n\n blem: ErxLen is %d shorter than default blocklen %d. Note that gCode is %d \n', p.rx.ErxLen, brutBlockLen, length(p.tx.gCode));
-    brutBlockLen = floor(p.tx.nbCodes*length(p.tx.gCode)/p.tx.subcarriers); 
-    fprintf('\nBlock length set to %d = nbCodes*%d/subcarriers \n', brutBlockLen, length(p.tx.gCode));
-elseif brutBlockLen<5*length(p.tx.gCode) %Check brutBlockLen>>hLen %FIXME
+if p.rx.ErxLen < brutBlockLen % If acquired or simulated backscaterring is too short (as if T_probing << 1s)
+    fprintf('\n\n blem: ErxLen is %d shorter than default blocklen %d. Note that gCode is %d \n', p.rx.ErxLen, brutBlockLen, length(p.rx.gCode));
+    brutBlockLen = floor(p.tx.nbCodes*length(p.rx.gCode)/p.tx.subcarriers); 
+    fprintf('\nBlock length set to %d = nbCodes*%d/subcarriers \n', brutBlockLen, length(p.rx.gCode));
+elseif brutBlockLen<5*length(p.rx.gCode) %Check brutBlockLen>>hLen % Not tested yet - EA 27/05/25
     fprintf('\nBlock length %d is too short for correlation per block procedure. Must be greater than hLen=%d. Quit\n', brutBlockLen, length(Erx));
 %     brutBlockLen = (p.tx.nbCodes-1)*length(gCode)-1;
-    brutBlockLen = 8*length(p.tx.gCode); 
+    brutBlockLen = 8*length(p.rx.gCode); 
 %     fprintf('\nBlock length set to %d = nbCodes*%d \n', brutBlockLen, length(gCode));
 %     %return;
 end
 
-p = fcorrAndGetJones(p.tx.gCode, brutBlockLen,p); %Processing per block
+p = fcorrAndGetJones(p.rx.gCode, brutBlockLen,p); %Processing per block
 fprintf('\nCode correlation AND Jones Matrix extraction processes completed within %.1f seconds. %d blocks of %d samples (>%d samples per code)\n', toc, floor(length(Erx)/(brutBlockLen-length(Erx)+1)), brutBlockLen-length(Erx)+1, length(Erx));
 
