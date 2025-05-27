6bf4968d0b939ca0c175cbc39cd98c77692f2bc3
Elie Awwad <elie.awwad@telecom-paris.fr>
2025-05-27 17:59:05 +0200

    Edit fcorrAndGetJones.m

--- functions/fcorrAndGetJones.m
+++ functions/fcorrAndGetJones.m
@@ -9,36 +9,58 @@ function p=fcorrAndGetJones(gCode, brutBlockLen, p)
 %
 % Authors: 
 % Original code by S. Guerrier, C. Dorize & E. Awwad - 2022
-% Modified version by E. Awwad - 2024 elie.awwad@telecom-paris.fr
+% Modified version by E. Awwad - 2025 elie.awwad@telecom-paris.fr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 global Erx;
 
-p.rx.ErxLen = brutBlockLen; 
-Erx = Erx(1:brutBlockLen,:); 
+%p.rx.ErxLen = brutBlockLen; 
+%Erx = Erx(1:brutBlockLen,:); 
 
-hLen  = length(gCode)/p.tx.ovsFactor;
+codeLen  = length(gCode);
+netBlockLen = brutBlockLen-codeLen+1; % -codeLen +1 to avoid the last code in the block that can be impacted by time aliasing
+nbFullBlocks = floor(p.rx.ErxLen/netBlockLen);
+nbRemainder = mod(p.rx.ErxLen,netBlockLen);
 
-% Process 1st block apart to include time synchro (Rayleigh start & stop positions) prior to Jones matrix extraction
-offsetInStart = 1; blockIdx = 1;
 
 % Code spectrum is zero-padded to reach length 'brutBlockLen'
 gCode_fft(:,1) = fft(gCode(1,:).',brutBlockLen);
 gCode_fft(:,2) = fft(gCode(2,:).',brutBlockLen);
 
-tmpCorr = fctCorrBlock(Erx(offsetInStart:1:offsetInStart+brutBlockLen-1,:), gCode_fft, hLen,p.rx.offset_ratio);
+Erx = ([Erx; zeros(codeLen-1,2)]);%Insert zeros at input array termination to avoid alloc error in edge calculation
 
-netBlockLen = length(tmpCorr);
-nbFullBlocks = floor((p.rx.ErxLen-(hLen-1))/netBlockLen);
-nbRemainder = mod(p.rx.ErxLen-(hLen-1),netBlockLen);
+
+% Process 1st block apart to include time synchro (Rayleigh start & stop positions) prior to Jones matrix extraction
+offsetInStart = 1; blockIdx = 1;
+tmpCorr = fctCorrBlock(Erx(offsetInStart:1:offsetInStart+brutBlockLen-1,:), gCode_fft, codeLen,p.rx.offset_ratio);
+
+%netBlockLen = length(tmpCorr);
+%nbFullBlocks = floor((p.rx.ErxLen-(codeLen-1))/netBlockLen);
+% nbRemainder = mod(p.rx.ErxLen-(codeLen-1),netBlockLen);
 
 %% Time synchronization: search for Rayleigh Start & Stop from the first processed subblock
 ovsCodeLength = p.rx.ovsFactor*p.tx.Ncode;%nb of samples between 2 consecutive codes in the current trace
-
 nbExpectedCodes = floor(length(tmpCorr)/ovsCodeLength);%nbExpectedCodes in the 1st subblock
 
-%sumAbsTmpCorr = tmpCorr(1,:).*conj(tmpCorr(1,:))+tmpCorr(2,:).*conj(tmpCorr(2,:))+tmpCorr(3,:).*conj(tmpCorr(3,:))+tmpCorr(4,:).*conj(tmpCorr(4,:));%Frobenius norm 
-sumAbsTmpCorr = abs(tmpCorr(1,:).*tmpCorr(4,:)+tmpCorr(2,:).*tmpCorr(3,:));% |det| criterion
+switch(p.ProbingMode)
+    
+    case 'MIMO'
+        sumAbsTmpCorr = abs(tmpCorr(1,:).*tmpCorr(4,:)+tmpCorr(2,:).*tmpCorr(3,:));% |det| criterion
+        %sumAbsTmpCorr = tmpCorr(1,:).*conj(tmpCorr(1,:))+tmpCorr(2,:).*conj(tmpCorr(2,:))+tmpCorr(3,:).*conj(tmpCorr(3,:))+tmpCorr(4,:).*conj(tmpCorr(4,:));%Frobenius norm 
+
+    case 'SIMO'
+        sumAbsTmpCorr = tmpCorr(1,:).*conj(tmpCorr(1,:))+tmpCorr(2,:).*conj(tmpCorr(2,:)); 
+        
+    case 'MISO' % Not tested
+        sumAbsTmpCorr = tmpCorr(1,:).*conj(tmpCorr(1,:))+tmpCorr(32,:).*conj(tmpCorr(3,:)); 
+        
+    case 'SISO' % Not tested
+        sumAbsTmpCorr = tmpCorr(1,:).*conj(tmpCorr(1,:)); 
+        
+    otherwise
+        error('Unsupported probing mode');
+        
+end
 
 if p.tx.detectionindex(1) == 0 && p.tx.detectionindex(2) == 0 %no detection conducted yet
     [xStartRay,xStopRay,estimatedFiberLength,p] = getRayleighSpread(sumAbsTmpCorr, ovsCodeLength, nbExpectedCodes, p); %Get Start & Stop positions of Rayleigh backscatter, processed here from the 1st block of data
@@ -51,10 +73,11 @@ end
 
 p.fibre.L = estimatedFiberLength;
 
-if nbRemainder<hLen %Useless to carry out the remaining truncated block if its size < hLen %FIXME not tested
-    p.HiTab = single(NaN(4,nbFullBlocks*p.rx.nbOvsReflectors*(nbExpectedCodes+1)));%CD: prealloc for speed issue, with an overevaluated table size
+%preallocate a space for the storage of Jones matrices. Create an over-sized table
+if nbRemainder<codeLen %Useless to carry out the remaining truncated block if its size < codeLen %FIXME not tested
+    p.HiTab = single(NaN(4,nbFullBlocks*p.rx.nbOvsReflectors*(nbExpectedCodes+1)));
 else
-    p.HiTab = single(NaN(4,(nbFullBlocks+1)*p.rx.nbOvsReflectors*(nbExpectedCodes+1)));%CD: prealloc for speed issue, with an overevaluated table size
+    p.HiTab = single(NaN(4,(nbFullBlocks+1)*p.rx.nbOvsReflectors*(nbExpectedCodes+1)));
 end
 rayleighBlockMask = getRayleighBlockMask(netBlockLen, blockIdx, xStartRay, xStopRay, ovsCodeLength);%Get the mask that localizes the series of Rayleigh Jones matrices contained in the current subblock 'tmpCorr'
 
@@ -67,6 +90,7 @@ fprintf('\nSpatial resolution (ovs): %.2fm   Estimated fibre length:%.1fm   Lase
 %% Process the next blocks to iteratively extract their Jones matrices
 for blockIdx=2 : nbFullBlocks %FIXME not tested yet
     offsetInStart = offsetInStart + netBlockLen; %input block translation is netBlockLen
+
 % B2B deconvolution prior to correlation
 %     if p.file.DECONV_B2B==1 ||  p.file.DECONV_B2B==2 %the 2 received channels are deconvolved with the same filter hB2B
 %         Erx(offsetInStart:offsetInStart+brutBlockLen-1,1) = ifft( fft(Erx(offsetInStart:offsetInStart+brutBlockLen-1,1))./sp_hB2B);
@@ -76,22 +100,23 @@ for blockIdx=2 : nbFullBlocks %FIXME not tested yet
 %         Erx(offsetInStart:offsetInStart+brutBlockLen-1,2) = ifft( fft(Erx(offsetInStart:offsetInStart+brutBlockLen-1,2))./sp_hB2B_2 );
 %     end
        
-    %%
-    tmpCorr = fctCorrBlock(Erx(offsetInStart+brutBlockLen-1:-1:offsetInStart,:), gCode_fft, hLen,p.rx.offset_ratio);
+    tmpCorr = fctCorrBlock(Erx(offsetInStart:1:offsetInStart+brutBlockLen-1,:), gCode_fft, codeLen,p.rx.offset_ratio);
     rayleighBlockMask = getRayleighBlockMask(netBlockLen, blockIdx, xStartRay, xStopRay, ovsCodeLength);%Get the mask that localizes the series of Rayleigh Jones matrices contained in the current subblock 'tmpCorr'
-    idxStart = idxStart+nbMaskElts; nbMaskElts = sum(rayleighBlockMask==1);
+    idxStart = idxStart+nbMaskElts; 
+    nbMaskElts = sum(rayleighBlockMask==1);
     p.HiTab(:,idxStart:idxStart+nbMaskElts-1) = tmpCorr(:,rayleighBlockMask==1);%Extract all the Rayleigh Jones matrices from the current block and store them in p.HiTab
 end
+clear gCode_fft; % The variable is cleared because it will be redefined in the upcoming part to process the remainder samples (shorter than a full block)
 
 %Process the remaining block (size<netBlockLen)
-if nbRemainder>=hLen %Useless to carry out the remaining truncated block if its size < hLen %FIXME not tested
+if nbRemainder>=codeLen %Useless to carry out the remaining truncated block if its size < codeLen %FIXME not tested
 
-    gCode_fft(:,1) = fft(gCode(1,:).',nbRemainder+hLen-1);%code spectrum zero-padded to reach length of processed block
-    gCode_fft(:,2) = fft(gCode(2,:).',nbRemainder+hLen-1);
+    gCode_fft(:,1) = fft(gCode(1,:).',nbRemainder);%code spectrum zero-padded to reach length of processed block
+    gCode_fft(:,2) = fft(gCode(2,:).',nbRemainder);
     offsetInStart = offsetInStart + netBlockLen; %input block translation is netBlockLen
-    clear tmpCorr; tmpCorr = single(zeros(4,nbRemainder));
+    clear tmpCorr; 
     
-%%%%
+%%%% Not tested lately
 % % for B2B deconvolution prior to correlation on the remaining received data block
 % if p.file.DECONV_B2B==1 ||  p.file.DECONV_B2B==2 %the 2 received channels are deconvolved with the same filter hB2B
 %         sp_hB2B = fft(hB2B, nbRemainder);%Spectral response after zero padding to match size of the received data block onto which the deconvolution will be applied
@@ -104,11 +129,12 @@ if nbRemainder>=hLen %Useless to carry out the remaining truncated block if its 
 %        Erx(offsetInStart:offsetInStart+nbRemainder-1,2) = ifft( fft(Erx(offsetInStart:offsetInStart+nbRemainder-1,2))./sp_hB2B_2 );
 % end  
 
-    tmpCorr = fctCorrBlockRev(Erx(offsetInStart+nbRemainder+hLen-1-1:-1:offsetInStart,:), gCode_fft, hLen);
+    tmpCorr = fctCorrBlock(Erx(offsetInStart:1:offsetInStart+nbRemainder-1,:), gCode_fft, codeLen,p.rx.offset_ratio);
     blockIdx = nbFullBlocks + 1;
-    rayleighBlockMask = getRayleighBlockMask(netBlockLen, blockIdx, xStartRay, xStopRay, ovsCodeLength);%Get the mask that localizes the series of Rayleigh Jones matrices contained in the current subblock 'tmpCorr'
-    idxStart = idxStart+nbMaskElts; nbMaskElts = sum(rayleighBlockMask(1:nbRemainder)==1);
-    p.HiTab(:,idxStart:idxStart+nbMaskElts-1) = tmpCorr(:,rayleighBlockMask(1:nbRemainder)==1);%Extract all the Rayleigh Jones matrices from the current block and store them in p.HiTab
+    rayleighBlockMask = getRayleighBlockMask(nbRemainder-codeLen, blockIdx, xStartRay, xStopRay, ovsCodeLength);%Get the mask that localizes the series of Rayleigh Jones matrices contained in the current subblock 'tmpCorr'
+    idxStart = idxStart+nbMaskElts; 
+    nbMaskElts = sum(rayleighBlockMask==1);
+    p.HiTab(:,idxStart:idxStart+nbMaskElts-1) = tmpCorr(:,rayleighBlockMask==1);%Extract all the Rayleigh Jones matrices from the current block and store them in p.HiTab
     p.HiTab = p.HiTab(:,1:idxStart+nbMaskElts-1); %remove the zeros
 else
     p.HiTab = p.HiTab(:,1:idxStart+nbMaskElts-1);
